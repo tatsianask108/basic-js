e5917dea80ebc936f20059885a44e291ada09a51
tatsianask108 <tt.laurie@gmail.com>
2023-12-13 23:03:47 +0300

    refactor: optimise ternary operator in count-cats task

--- src/count-cats.js
+++ src/count-cats.js
@@ -21,7 +21,7 @@ function countCats(matrix) {
   for (let i = 0; i < matrix.length; i++) {
 
     for (let j = 0; j < matrix[i].length; j++) {
-      matrix[i][j] === '^^' ? sum += 1 : sum += 0;
+      matrix[i][j] === '^^' ? sum += 1 : sum;
     }
 
   }
