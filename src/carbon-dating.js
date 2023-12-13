834953435b08cf81305d20d1b6d8cad41e955608
tatsianask108 <tt.laurie@gmail.com>
2023-12-13 14:54:50 +0300

    feat: solve carbon-dating task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -18,7 +18,11 @@ const HALF_LIFE_PERIOD = 5730;
  *
  */
 function dateSample(sampleActivity) {
-
+  if (typeof (sampleActivity) !== 'string' || isNaN(parseInt(sampleActivity)) || +sampleActivity <= 0) return false;
+  const k = 0.693 / HALF_LIFE_PERIOD;
+  const t = Math.ceil((Math.log(MODERN_ACTIVITY / sampleActivity)) / k);
+  if (t <= 0) return false;
+  return t;
 }
 
 module.exports = {
